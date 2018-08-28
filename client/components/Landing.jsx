13a47f0c2b43d4d8544ee06b34cb2d4b6b42ccb5
Jordan Bice <jmbice@gmail.com>
2018-08-28 06:41:33 -0700

    Finishing google api, refactoring further

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -218,6 +218,61 @@ class Landing extends React.Component {
     });
   }
 
+  const googleTravelTimes = (departureTime) => {
+    const origins = [];
+    const destinations = [];
+    rbcAppointments.forEach((apt) => {
+      const bookedLocations = `${apt.address.split(' ').join('+')}+${apt.city.split(' ').join('+')}+${apt.state.split(' ').join('+')}+${apt.zip.split(' ').join('+')}`;
+      const potentialLocation = `${address.split(' ').join('+')}+${city.split(' ').join('+')}+${state.split(' ').join('+')}+${zip.split(' ').join('+')}`;
+      origins.push(potentialLocation);
+      destinations.push(bookedLocations);
+      origins.push(bookedLocations);
+      destinations.push(potentialLocation);
+    });
+
+    const orgString = origins.join('|');
+    const destString = destinations.join('|');
+    const locationsString = `?origins=${orgString}&destinations=${destString}`;
+    const paramatersString = `&mode=driving&language=en-EN&key=${key.API_KEY}`;
+    let completeString = '';
+
+    if (departureTime) {
+      const trafficParamsString = `&mode=driving&departure_time=${departureTime}&traffic_model=best_guess&language=en-EN&key=${key.API_KEY}`;
+      completeString = `https://maps.googleapis.com/maps/api/distancematrix/json${locationsString}${trafficParamsString}`;
+    } else {
+      completeString = `https://maps.googleapis.com/maps/api/distancematrix/json${locationsString}${paramatersString}`;
+    }
+
+    //console.log(completeString);
+    // $.ajax({
+    //   url: 'http://localhost:3000/api/provider/travelTimes',
+    //   method: 'POST',
+    //   data: { googleQuery: completeString },
+    //   dataType: 'json',
+    //   success: (data) => {
+    //     console.log('Response:', data);
+    //   },
+    // });
+  };
+  //googleTravelTimes();
+
+  const adjustAppointments = () => {
+    const newAppointments = [];
+    appointments.forEach((apt, index) => {
+      const newEvent = {};
+      const before = index * 2;
+      const after = before + 1;
+      const durationBefore = Math.ceil(googleData.rows[before].elements[before].duration.value / 60);
+      const durationAfter = Math.ceil(googleData.rows[after].elements[after].duration.value / 60);
+      newEvent.duration = (Number(apt.duration) + durationBefore + durationAfter).toString();
+      newEvent.start_time = moment(apt.start_time, 'HH:mm');
+      newEvent.start_time.subtract(durationBefore, 'minutes');
+      newEvent.start_time = newEvent.start_time.format('HH:mm');
+      newAppointments.push(newEvent);
+    });
+    return newAppointments;
+  };
+
   formHandler(e) {
     const field = e.target.name;
     const target = e.target.value;
